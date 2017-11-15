d6c4919d3f0a7722c544ac43c73c4351f46d349f
manjeetchauhan211 <manjeetchauhan211@gmail.com>
2017-11-16 01:00:23 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -7,8 +7,8 @@ provider "aws" {
   access_key = "${var.AWS_ACCESS_KEY}"
   secret_key = "${var.AWS_SECRET_KEY}"
 }
-resource "aws_key_pair" "new-key" {
-  key_name = "new-key"
+resource "aws_key_pair" "new-key1" {
+  key_name = "new-key1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDm9BGj0exTWGm5MHihLP2qXh6jufo+Yt0KXKjZEzf+rKH4yRkVvLU+4RAAW8nuRR+Lb4GilJrwEjvqXs2HgGv+yAgIj/qj//BI/OQbPG5nzlpDX/o9x2jCC3MJFYKSRjH/eAWKkq2/d5LsWan+0wuIaQC6BLoUFEwDcVdCnDbd+RwaxnI4ADMhEZwXo3IJx1o4PwQF51+ZVo1kyZL32qVy7CCRBZ0TKOQoibM4vSCJA8JDuYpQvdEauDPuLtYba+lIk4oZH7pDSUwboZPs2nGKQ9GY7ccGqeF+1s8Ve1li20A12MF7WW5T373FiBy0PDDOJ4kRvsXboRYcDs2rVtsV root@ip-172-31-47-90" 
 }
 
@@ -16,7 +16,7 @@ resource "aws_instance" "web" {
    count = 2
    ami = "ami-da05a4a0"
    instance_type = "t2.micro"
-   key_name = "${aws_key_pair.new-key.key_name}"
+   key_name = "${aws_key_pair.new-key1.key_name}"
    tags {
       Name = "Test-instance"
     }
