c32a8fde80d29eab69d2a42a3822552cd6bcb29e
manjeetchauhan211 <manjeetchauhan211@gmail.com>
2018-02-11 12:49:25 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -8,12 +8,12 @@ provider "aws" {
   secret_key = "${var.AWS_SECRET_KEY}"
 }
 resource "aws_key_pair" "new-key1" {
-  key_name = "new-key1"
+  key_name = "new-zone"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDm9BGj0exTWGm5MHihLP2qXh6jufo+Yt0KXKjZEzf+rKH4yRkVvLU+4RAAW8nuRR+Lb4GilJrwEjvqXs2HgGv+yAgIj/qj//BI/OQbPG5nzlpDX/o9x2jCC3MJFYKSRjH/eAWKkq2/d5LsWan+0wuIaQC6BLoUFEwDcVdCnDbd+RwaxnI4ADMhEZwXo3IJx1o4PwQF51+ZVo1kyZL32qVy7CCRBZ0TKOQoibM4vSCJA8JDuYpQvdEauDPuLtYba+lIk4oZH7pDSUwboZPs2nGKQ9GY7ccGqeF+1s8Ve1li20A12MF7WW5T373FiBy0PDDOJ4kRvsXboRYcDs2rVtsV root@ip-172-31-47-90" 
 }
 
 resource "aws_instance" "web" {
-   count = 1
+   count = 2
    ami = "ami-da05a4a0"
    instance_type = "t2.micro"
    key_name = "${aws_key_pair.new-key1.key_name}"
